e477f2c067770d60a72f45595c14f8817c9ccb61
julianatonu <julianatonu@gmail.com>
2024-01-18 13:48:19 +0600

    add

--- index.js
+++ index.js
@@ -176,6 +176,14 @@ app.delete('/menu/:id', async(req,res)=>{
 app.post('create-payment-intent', async(req,res)=>{
   const{price}=req.body;
   const amount =price*100;
+  const paymentIntent =await stripe.paymentIntents.create({
+    amount:amount,
+    currency:'usd',
+    payment_method_types:['card']
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
   
 })
 
